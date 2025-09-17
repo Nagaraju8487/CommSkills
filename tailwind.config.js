b6f9c0081e70028dc2d0632537fc089cd9316711
Nagaraju8487 <guthulanagaraju2004@gmail.com>
2025-09-17 15:42:24 +0530

    Resolve merge conflict in vite.config.ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -7,13 +7,4 @@ export default {
   },
   plugins: [],
 };
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
->>>>>>> 9ba9e94e02e60f1d12d77b039442d66365d1798e
+
